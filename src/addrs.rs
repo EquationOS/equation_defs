8ca72dc5225917a0a98d7489a39a7eb3440ce4b0
hky1999 <976929993@qq.com>
2025-05-02 13:33:32 +0800

    Initial commit

---
+++ src/addrs.rs
@@ -0,0 +1,47 @@
+use axaddrspace::{GuestPhysAddr, GuestVirtAddr};
+use memory_addr::PAGE_SIZE_1G;
+
+/* Guest Process Virtual Address Space Layout (in GVA).*/
+
+/// Instance shared region base address in GVA.
+/// This is a percpu specific region, shared by all instances on the same CPU.
+pub const INSTANCE_SHARED_REGION_BASE_GVA: GuestVirtAddr =
+    GuestVirtAddr::from_usize(0xffff_ff00_0000_0000);
+
+/// Instance inner region base address in GVA.
+/// This is a instance specific region, shared by all processes in the same instance.
+pub const INSTANCE_INNER_REGION_BASE_GVA: GuestVirtAddr =
+    GuestVirtAddr::from_usize(0xffff_ff00_0000_1000);
+
+/// Process inner region base address in GVA.
+/// This is a process specific region, shared by all threads in the same process.
+pub const PROCESS_INNER_REGION_BASE_GVA: GuestVirtAddr =
+    GuestVirtAddr::from_usize(0xffff_ff00_0000_2000);
+
+/// Guest Process's GVA view of the EPTP list region on current CPU, only mapped in gate processes.
+pub const GP_EPT_LIST_REGION_GVA: GuestVirtAddr = GuestVirtAddr::from_usize(0xffff_ff00_0000_3000);
+
+/*  Guest Process Physical Address Space Layout (in GPA).*/
+
+/// Base address in GPA of instance shim.
+/// Can it be obtained from shim's config file?
+pub const SHIM_BASE_GPA: GuestPhysAddr = GuestPhysAddr::from_usize(0x0);
+/// Guest Process's GPA view of the guest page table, which will be set as the process's CR3.
+pub const GUEST_PT_ROOT_GPA: GuestPhysAddr = GuestPhysAddr::from_usize(0xff80_0000_0000);
+
+/// Instance shared region base address in GPA.
+pub const INSTANCE_SHARED_REGION_BASE_GPA: GuestPhysAddr =
+    GuestPhysAddr::from_usize(0xff00_0000_0000);
+/// Instance inner region base address in GPA.
+pub const INSTANCE_INNER_REGION_BASE_GPA: GuestPhysAddr =
+    GuestPhysAddr::from_usize(0xff00_0000_1000);
+/// Process inner region base address in GPA.
+pub const PROCESS_INNER_REGION_BASE_GPA: GuestPhysAddr =
+    GuestPhysAddr::from_usize(0xff00_0000_2000);
+/// Guest Process's GPA view of the EPTP list region on current CPU, only mapped in gate processes.
+pub const GP_EPTP_LIST_REGION_BASE_GPA: GuestPhysAddr = GuestPhysAddr::from_usize(0xff00_0000_3000);
+
+/// (Only used for coarse-grained segmentation mapping)
+///
+/// Guest Process first region base address.
+pub const GUEST_MEM_REGION_BASE: GuestPhysAddr = GuestPhysAddr::from_usize(PAGE_SIZE_1G);
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![no_std]
+
+mod addrs;
+mod header;
+
+pub use addrs::*;
+pub use header::*;
